46e8a5a3cb5dccb077949da265fa219cf739de61
Francois Perron <fperron@gmail.com>
2020-12-01 12:54:45 -0500

    Use <for ... of> for fun

--- src/01/day01.js
+++ src/01/day01.js
@@ -1,19 +1,19 @@
 const reportRepair = entries => {
-  for (let i = 0; i < entries.length; i++) {
-    for (let j = 0; j < entries.length; j++) {
-      if (entries[i] + entries[j] === 2020) {
-        return entries[i] * entries[j]
+  for (const e1 of entries) {
+    for (const e2 of entries) {
+      if (e1 + e2 === 2020) {
+        return e1 * e2
       }
     }
   }
 }
 
 const reportRepairPart2 = entries => {
-  for (let i = 0; i < entries.length; i++) {
-    for (let j = 0; j < entries.length; j++) {
-      for (let k = 0; k < entries.length; k++) {
-        if (entries[i] + entries[j] + entries[k] === 2020) {
-          return entries[i] * entries[j] * entries[k]
+  for (const e1 of entries) {
+    for (const e2 of entries) {
+      for (const e3 of entries) {
+        if (e1 + e2 + e3 === 2020) {
+          return e1 * e2 * e3
         }
       }
     }
